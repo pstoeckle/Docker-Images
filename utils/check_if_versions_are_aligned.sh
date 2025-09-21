a3dd832d07e7afb63800d9aa99abc5b96d5aa61f
Patrick Stoeckle <patrick.stoeckle@posteo.de>
2025-09-21 22:00:26 +0200

    :construction_worker: chore(ci): add script for consistency

---
+++ utils/check_if_versions_are_aligned.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -eu
+RUBY_VERSION_IN_DOCKERFILE=$(grep -oE 'ENV RUBY_VERSION=([0-9]+\.[0-9]+\.[0-9]+)' node-ruby/Dockerfile | cut -d'=' -f2)
+readonly RUBY_VERSION_IN_DOCKERFILE
+
+NODE_VERSION_IN_DOCKERFILE=$(grep -oE 'FROM node:([0-9]+\.[0-9]+\.[0-9]+)' node-ruby/Dockerfile | cut -d':' -f2 | cut -d'-' -f1)
+readonly NODE_VERSION_IN_DOCKERFILE
+
+COMBINED="${NODE_VERSION_IN_DOCKERFILE}--${RUBY_VERSION_IN_DOCKERFILE}"
+COMBINED="${COMBINED/ /}"
+readonly COMBINED
+
+COMBINED_IN_CI=$(grep -oE 'image-tag:\s+([0-9]+\.[0-9]+\.[0-9]+--[0-9]+\.[0-9]+\.[0-9]+)' .github/workflows/ci.yml | cut -d':' -f2 )
+COMBINED_IN_CI="${COMBINED_IN_CI/ /}"
+readonly COMBINED_IN_CI
+
+if [ "$COMBINED_IN_CI" = "$COMBINED" ]; then
+    echo "Everything in sync"
+else
+    echo "$COMBINED_IN_CI != $COMBINED"
+    exit 1
+fi
